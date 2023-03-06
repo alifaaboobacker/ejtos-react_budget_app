6f26467f3619f72685821fc988f18b872188ecd7
Alifa Aboobacker <71171175+alifaaboobacker@users.noreply.github.com>
2023-03-06 13:40:18 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,32 +1,47 @@
 import React from 'react';
-
 import 'bootstrap/dist/css/bootstrap.min.css';
-
-//Code to import Budget.js
-import Budget from './components/Budget';
-
-// Add code to import the other components here under
-
-
+// Add code to import the components
 import { AppProvider } from './context/AppContext';
+import Budget from './components/Budget';
+import ExpenseTotal from './components/ExpenseTotal';
+import ExpenseList from './components/ExpenseList';
+import AllocationForm from './components/AllocationForm';
+import RemainingBudget from './components/Remaining';
+import Currency from './components/Currency';
+
 const App = () => {
     return (
         <AppProvider>
             <div className='container'>
-                <h1 className='mt-3'>Company's Budget Allocation</h1>
-                    <div className='row mt-3'>
-                        {/* Add Budget component here under */}        
-
-                        {/* Add Remaining component here under */}        
-
-                        {/* Add ExpenseTotal component here under */}        
-                       
-                        {/* Add ExpenseList component here under */}         
-
-                        {/* Add ExpenseItem component here under */}        
-
-                        {/* Add AllocationForm component here under */}        
-
+                {/* Add the code here to add the components*/}
+             
+                <h1 className='mt-3'>Budget Allocation</h1>
+                
+                <div className='row mt-3'>
+                    <div className='col-sm'>
+                        <Budget />
+                    </div>
+                    <div className='col-sm'>
+                        <RemainingBudget />
+                    </div>
+                    <div className='col-sm'>
+                        <ExpenseTotal />
+                    </div>
+                    <div className='col-sm'>
+                        <Currency />
+                    </div>
+                </div>
+                <h3 className='mt-2 bg-secondary text-light'>Allocation</h3>
+                <div className='row'>
+                    <div className='col-sm bg-light'>
+                        <ExpenseList />
+                    </div>
+                </div>
+                <h3 className='mt-1 bg-secondary text-light'>Change allocation</h3>
+                <div className='row mt-3'>
+                    <div className='col-sm bg-light'>
+                        <AllocationForm/>
+                    </div>
                 </div>
             </div>
         </AppProvider>
